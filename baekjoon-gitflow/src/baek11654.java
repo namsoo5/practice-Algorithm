f06f780dcf6e0c45dc172eabc8f0bd3f9c285411
namsoo <are34@naver.com>
2019-04-23 00:21:57 +0900

    아스키코드변환

---
+++ baekjoon-gitflow/src/baek11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//아스키코드 변환
+public class baek11654 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String a = scan.nextLine();
+        char[] word = a.toCharArray();
+
+        System.out.println((int)word[0]);
+
+    }
+}
